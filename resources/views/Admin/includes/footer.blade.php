c51c0992db0d2a09f5e17034426b9a21442f64c3
ANTAR NANDI <antarnandy.2@gmail.com>
2022-02-02 10:36:02 +0600

    sidebar form fixed

--- resources/views/Admin/includes/footer.blade.php
+++ resources/views/Admin/includes/footer.blade.php
@@ -14,3 +14,21 @@
 <script src="{{ asset('Real/lib/owlcarousel/owl.carousel.min.js')}}"></script>
 <script src="{{ asset('Real/lib/scrollreveal/scrollreveal.min.js')}}"></script>
 <script src="{{ asset('Real/js/main.js')}}"></script>
+
+
+
+
+<!--/ Nav End /-->
+<!-- JavaScript Libraries -->
+<script src="{{ asset('lib/jquery/jquery.min.js')}}"></script>
+<script src="{{ asset('lib/jquery/jquery-migrate.min.js')}}"></script>
+<script src="{{ asset('lib/popper/popper.min.js')}}"></script>
+<script src="{{ asset('lib/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{ asset('lib/easing/easing.min.js')}}"></script>
+<script src="{{ asset('lib/owlcarousel/owl.carousel.min.js')}}"></script>
+<script src="{{ asset('lib/scrollreveal/scrollreveal.min.js')}}"></script>
+<!-- Contact Form JavaScript File -->
+<script src="{{ asset('contactform/contactform.js')}}"></script>
+
+<!-- Template Main Javascript File -->
+<script src="{{ asset('js/main.js')}}"></script>')}}
--- resources/views/Admin/includes/head.blade.php
+++ resources/views/Admin/includes/head.blade.php
@@ -13,4 +13,19 @@
 <!-- Assets Folder Css -->
 <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/all.min.css')}}">
 <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
+<link rel="stylesheet" href="{{ asset('style.css')}}">
+<link href="{{ asset('animate.min.css')}}" rel="stylesheet">
 <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-datetimepicker.min.css')}}">
+
+<!-- Real strnatcasecmp -->
+<!-- Bootstrap CSS File -->
+<link href="{{ asset('lib/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+<!-- Libraries CSS Files -->
+<link href="{{ asset('lib/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+<link href="{{ asset('lib/animate/animate.min.css')}}" rel="stylesheet">
+<link href="{{ asset('lib/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
+<!-- <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet"> -->
+
+<!-- Main Stylesheet File -->
+<link href="{{ asset('css/style.css')}}" rel="stylesheet">
